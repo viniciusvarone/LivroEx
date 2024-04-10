6797bbbc16b42704f09ad6a2c8c30e86ba3024d3
viniciusvarone <139243276+viniciusvarone@users.noreply.github.com>
2024-04-10 18:41:32 -0300

    html6.2

---
+++ Cap.6/ex6.2/script.js
@@ -0,0 +1,5 @@
+const frm = document.querySelector('form');
+const respErros = document.getElementById('erros');
+const respChances = document.getElementById('chances');
+const respDicas = document.getElementById('dicas');
+
