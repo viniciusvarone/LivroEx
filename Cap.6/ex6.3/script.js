54a9942484f42ece91cb3a656a74f4545a5d18ca
viniciusvarone <139243276+viniciusvarone@users.noreply.github.com>
2024-05-30 20:27:49 -0300

    btListar

--- Cap.6/ex6.3/script.js
+++ Cap.6/ex6.3/script.js
@@ -12,5 +12,21 @@ frm.addEventListener("submit", (e)=>{
     frm.entPreco.value = ""
     frm.entModel.focus()
 
-    console.log(carros)
+    //dispara um evento clique no botão de listar (como se o usuário tivesse clicado) 
+    frm.btListar.dispatchEvent(new Event("click")) 
 })
+
+//função do btListar
+frm.btListar.addEventListener("click", ()=>{
+    //verificação para ver se há algum objeto na lista a ser apresentado
+    if (carros.length == 0){
+        alert('Não há carros na lista')
+        return
+    }
+
+    const Cadastrados = carros.reduce((acumulador, carro) => 
+        acumulador + carro.modelo + " - R$: " + carro.preco.toFixed(2) + "\n", "")
+
+    saida.innerText = `Lista de carros cadastrados: \n ${"-".repeat(40)} \n ${Cadastrados}`
+})
+
