b1409ecf7458323218da23d05fe63e523cf98f22
viniciusvarone <139243276+viniciusvarone@users.noreply.github.com>
2024-05-31 12:17:38 -0300

    RevendaHerbie

--- Cap.6/ex6.3/script.js
+++ Cap.6/ex6.3/script.js
@@ -16,6 +16,7 @@ frm.addEventListener("submit", (e)=>{
     frm.btListar.dispatchEvent(new Event("click")) 
 })
 
+
 //função do btListar
 frm.btListar.addEventListener("click", ()=>{
     //verificação para ver se há algum objeto na lista a ser apresentado
@@ -28,5 +29,23 @@ frm.btListar.addEventListener("click", ()=>{
         acumulador + carro.modelo + " - R$: " + carro.preco.toFixed(2) + "\n", "")
 
     saida.innerText = `Lista de carros cadastrados: \n ${"-".repeat(40)} \n ${Cadastrados}`
+})
+
+
+//função do btFiltrar
+frm.btFiltrar.addEventListener("click", ()=>{
+    const MaxPrice = prompt('Qual o preço máximo que o cliente deseja pagar?')
+    //cria um vetor com os carros de valor igual ou inferior ao desejado pelo cliente
+    const CarrosFiltrados = carros.filter(carro => carro.preco <= MaxPrice)
+    if (CarrosFiltrados.length == 0){
+        alert('Não há carros com preço inferior ou igual ao solicitado')
+        return
+    }
+
+    const ExibirFiltragem = CarrosFiltrados.reduce((acumulador, carro) =>
+        acumulador + carro.modelo + "- R$: " + carro.preco.toFixed(2) + "\n", "")
+
+    saida.innerText = `Lista de carros cadastrados: \n ${"-".repeat(40)} \n ${ExibirFiltragem}`
+
 })
 
